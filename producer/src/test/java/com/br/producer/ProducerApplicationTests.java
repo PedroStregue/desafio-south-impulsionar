e927712cc6bc969163292e6057d117cd7459b634
PedroStregue <pedrostregue@gmail.com>
2023-01-13 13:43:48 -0300

    criação do projeto bonus #2

---
+++ producer/src/test/java/com/br/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
